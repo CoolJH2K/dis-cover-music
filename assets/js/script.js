9d71db418a37193b14d39e9bd35273021efc1a7b
David Peguero <david.peguero.cancel@gmail.com>
2023-04-12 10:04:18 -0400

    Added format for the loop for videos

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,7 +8,26 @@ bulmaCarousel.attach('#carousel-demo', {
 //Function that extracts search results 
 async function extractYoutubeResults(){
     var searchResults = await getYoutubeList([`Linkin Park`, `In the End`, `Metal`, `Cover`]);
-    console.log(searchResults);
+    var videos = searchResults.list;
+
+    videos.forEach(video => {
+        
+    });
+    // <div class="item-1 m-1">
+    //     <!-- Slide Content -->
+    //     <div class="card">
+    //     <div class="card-image">
+    //         <figure class="image is-4by3">
+    //         <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
+    //         </figure>
+    //     </div>
+    //     <div class="card-content">
+    //         <!-- Add Youtube Data To Display here (?) -->
+    //         <p class="">Sample title</p>
+    //     </div>
+    //     </div>
+    // </div>
+    
 }
 
 extractYoutubeResults();
