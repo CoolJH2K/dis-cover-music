cac73aec2a4276c9618f661e2d8b40d1612d8a32
codemonkeyspoon <98781257+codemonkeyspoon@users.noreply.github.com>
2023-04-13 14:06:10 -0400

    proper index

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,6 +1,6 @@
 const button = document.querySelector('.button');
 
 button.addEventListener('click', function() {
-  window.location.href = 'index.html';
+  window.location.href = 'main.html';
 });
 
